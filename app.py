1d870dc4f8abf42e7a805d123efc1d8819c299a9
muthurajaji2 <muthurajaji2@gmail.com>
2026-05-31 15:09:44 +0530

    Updated Day 4 message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Day 4 Automatic Kubernetes Deployment 🚀 ready hiiii iam back"
+    return "🔥 Day 4 Automatic Kubernetes Deployment Awesome! 🚀 CI/CD Pipeline Working Successfully ✅"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
