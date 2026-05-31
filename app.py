2b2d46e4b741a8c93ceb77d1f21b85b24ad6f753
muthurajaji2 <muthurajaji2@gmail.com>
2026-06-01 00:10:05 +0530

    Updated Day 4 message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🔥 Day 6 Automatic Kubernetes Deployment Awesome! 🚀 CI/CD Pipeline Working Successfully ✅ HELM"
+    return "argocd"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
