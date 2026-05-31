94c471624f113aff5285855a2158daab1715e21c
muthurajaji2 <muthurajaji2@gmail.com>
2026-05-31 15:04:58 +0530

    Day 4 CD Pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Day 4 Automatic Kubernetes Deployment 🚀 ready"
+    return "Day 4 Automatic Kubernetes Deployment 🚀 ready hiiii"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
